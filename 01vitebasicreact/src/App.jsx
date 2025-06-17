c295b1673c5a8237938f22244a706141918bb635
subho1st <rajdeepfirst@gmail.com>
2025-06-17 10:29:23 +0530

    changes

---
+++ 01vitebasicreact/src/App.jsx
@@ -0,0 +1,13 @@
+import Subhead from "./Subhead.jsx";
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Subhead />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitebasicreact/src/Subhead.jsx
@@ -0,0 +1,7 @@
+function Subhead() {
+    return (
+        <h3>Hello Subho</h3>
+    )
+}
+
+export default Subhead;
